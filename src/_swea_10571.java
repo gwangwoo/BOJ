40b4bbfe392ad875a88cd8487fe79f15c218666e
gwangwoo <rhkddn5161@naver.com>
2020-10-05 20:18:25 +0900

    swea 10571

---
+++ src/_swea_10571.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class _swea_10571 {
+	static int map[][];
+	static int N,M;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int tc = 1; tc <= T ; tc++) {
+			N = sc.nextInt();
+			M = sc.nextInt();
+			map = new int[N][M];
+			for(int i = 0 ; i < N ; i++) {
+				for(int j = 0 ; j < M ; j++) {
+					map[i][j] = sc.nextInt();
+				}
+			}
+			int r_max[] = new int[N];
+			int c_max[] = new int[M];
+			for(int i = 0 ; i < N ; i++) {
+				for(int j = 0 ; j < M ; j++) {
+					r_max[i] = Math.max(r_max[i], map[i][j]);
+				}
+			}
+			
+			for(int i = 0 ; i < M ; i++) {
+				for(int j = 0 ; j < N ; j++) {
+					c_max[i] = Math.max(c_max[i], map[j][i]);
+				}
+			}
+		
+			
+			for(int i = 0 ; i < N ; i++) System.out.print(r_max[i] + " ");
+			System.out.println();
+			for(int i = 0 ; i < M ; i++) System.out.print(c_max[i] + " ");;
+			boolean flag = false;
+			for(int i = 0 ; i < N ; i++) {
+				for(int j = 0 ; j < M ; j++) {
+					if(map[i][j] == r_max[i] || map[i][j] == c_max[j]) continue;
+					flag = true;
+					break;
+				}
+				if(flag) break;
+			}
+			if(flag) System.out.println("#"+tc + " NO");
+			else System.out.println("#"+tc + " YES");
+		
+		}
+	}
+}
