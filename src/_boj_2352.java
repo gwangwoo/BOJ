115c3c298748d47f91378c74d36479bb987e2917
gwangwoo <rhkddn5161@naver.com>
2021-03-26 23:56:06 +0900

    반도체설계

--- src/_boj_2352.java
+++ src/_boj_2352.java
@@ -1,32 +1,46 @@
 import java.io.*;
 import java.util.*;
 public class _boj_2352 {
-	static int N;
-	static int arr[];
+	
 	static int dp[];
-	public static void main(String[] args) throws IOException {
+	static int arr[];
+	static int N;
+
+	static int lowerBound(int target, int idx) {
+		int left = 0;
+		int right = idx;
+		int ret = -1;
+		while(left <= right) {
+			int mid = (left + right) >> 1;
+			if(dp[mid] < target) {
+				left = mid+1;
+			}else {
+				right = mid-1;
+				ret = mid;
+			}
+		}
+		return ret; 
+	}
+	
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		arr = new int[N];
 		dp = new int[N];
-		for(int i = 0 ; i < N ; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+		arr = new int[N];
+		for(int i = 0 ; i < N ; i++) arr[i] = Integer.parseInt(st.nextToken())+1_000_000_000;
 		
-		for(int i = 0 ; i < N ; i++) {
-			if(dp[i] == 0) dp[i] = arr[i];
-			for(int j = i ; j < N ; j++) {
-				if(arr[i] < arr[j]) {
-					if(dp[j] < dp[i] + 1) {
-						dp[j] = dp[i] + 1;
-					}
-				}
+		dp[0] = arr[0];
+		int idx = 0;
+		for(int i = 1 ; i < N ; i++) {
+			if(dp[idx] < arr[i]) {
+				dp[++idx] = arr[i];
+			}else {
+				int val = lowerBound(arr[i], idx);
+				dp[val] = arr[i];
 			}
 		}
-		for(int i = 0 ; i < N ; i++) {
-			System.out.print(dp[i]+ " ");
-		}
+		System.out.println(idx+1);
 		
 	}
 
