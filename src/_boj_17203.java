1e0873d09b0cae6a3dfdd27214d7aec41f3da05d
gwangwoo <rhkddn5161@naver.com>
2021-08-03 22:55:40 +0900

    EasyMax

--- src/_boj_17203.java
+++ src/_boj_17203.java
@@ -2,28 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class _boj_17203 {
-	static int N,M;
+	static int N,Q;
 	static int arr[];
-	
-	public static void main(String[] args) throws IOException{
+	static int psum[];
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N];
+		Q = Integer.parseInt(st.nextToken());
+		psum = new int[N+1];
+		arr = new int[N+1];
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0 ; i < N ; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+		for(int i = 1 ; i<= N ; i++) arr[i] = Integer.parseInt(st.nextToken());
+		for(int i = 2 ; i <= N ; i++) {
+			psum[i] = psum[i-1] + Math.abs(arr[i] - arr[i-1]);
 		}
-		while(M-- > 0) {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for(int i = 0 ; i < Q ; i++) {
 			st = new StringTokenizer(br.readLine());
-			int l = Integer.parseInt(st.nextToken())-1;
-			int r = Integer.parseInt(st.nextToken())-1;
-			int sum = 0;
-			for(int i = l+1 ; i <= r ; i++) {
-				sum += Math.abs(arr[i]-arr[i-1]);
-			}
-			System.out.println(sum);
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			bw.write(psum[e]-psum[s]+"\n");
 		}
+		bw.flush();
+		bw.close();
 	}
 }
